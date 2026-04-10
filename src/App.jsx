542a5cba721a8b399a02ddb9a1662dda9824def7
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-04-11 00:16:30 +0530

    pages comp created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import {Card,CardContent,CardDescription,CardHeader,CardTitle,} from "@/components/ui/card"
+// import {Card,CardContent,CardDescription,CardHeader,CardTitle,} from "@/components/ui/card"
+
+import HomePage from "Components/HomePage"
+import { Routes,Route } from "react-router"
+
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/login" element={<Login></Login>} ></Route>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+
+    </Routes>
   )
 }
 
