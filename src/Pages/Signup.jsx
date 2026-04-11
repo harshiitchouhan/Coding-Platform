263b66739cbcf378f02074e5d08a76d556a96516
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-04-11 13:32:48 +0530

    React-Hook

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,32 +1,37 @@
-import { GalleryVerticalEnd } from "lucide-react"
-import { SignupForm } from "@/components/signup-form"
+import { useForm } from "react-hook-form";
+
 
 function Signup (){
+
+
+  const {register,handleSubmit,formState: { errors },} = useForm();
+  
+  function submitForm(data){
+    console.log(data);
+
+  }
+
     return(
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
+      <>
+      <form onSubmit={handleSubmit({submitForm})} >
+        
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" {...register, "name"}/>
         </div>
-      <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <SignupForm />
-          </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input  {...register, "email"}/>
         </div>
-      </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
-    </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input  {...register, "password"}/>
+        </div>
+
+      </form>
+      </>
   )
 
 }
