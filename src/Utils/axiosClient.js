64cafa7ffbbe28ca36bc84f90b566e887146a678
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-05-09 18:54:11 +0530

    changes

--- src/Utils/axiosClient.js
+++ src/Utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"; 
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
+   baseURL: "https://your-backend-vercel-url.vercel.app",
   withCredentials : true ,         // attach cookies/token
   headers: { 
     "Content-Type": "application/json"
