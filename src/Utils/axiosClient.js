fea2b4ad9f128786fceea47c35838b7745a12fd5
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-05-16 19:21:45 +0530

    base

--- src/Utils/axiosClient.js
+++ src/Utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://coding-platform-backend.vercel.app/",
   // baseURL: "https://coding-platform-backend-qx2iafnqx-harshit-s-projects2.vercel.app",
   withCredentials : true ,         // attach cookies/token
   headers: { 
